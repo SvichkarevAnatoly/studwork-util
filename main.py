8034ef1dc6ba15b37a410f70ee9a61604dcd2165
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-04-20 22:15:32 +0300

    рефакторинг - использование функции

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
-ratings = list()
-prices = list()
+def inputStatistics():
+    ratings = list()
+    prices = list()
 
-i = 0
-price = 1
-while price != 0:
-    print("Исполнитель ", i + 1, ":", sep="")
-    price = input("Введите предлагаемую цену: ")
-    if not price:
-        break
-    price = int(price)
-    rating = int(input("Введите рейтинг: ")) + 1
-    prices.append(price)
-    ratings.append(rating)
-    i += 1
-    print()
+    i = 0
+    price = 1
+    while price != 0:
+        print("Исполнитель ", i + 1, ":", sep="")
+        price = input("Введите предлагаемую цену: ")
+        if not price:
+            print("Конец ввода")
+            print()
+            break
+        price = int(price)
+        rating = int(input("Введите рейтинг: ")) + 1
+        prices.append(price)
+        ratings.append(rating)
+        i += 1
+        print()
 
-amount = 0
-amountRating = 0
-for price, rating in zip(prices, ratings):
-    amount += rating * price
-    amountRating += rating
+    return prices, ratings
 
-polinaRating = int(input("Рейтинг Полины: "))
 
-optPrice = (((amount * (amountRating + polinaRating)) / amountRating) - amount) / polinaRating
-optPrice = int(optPrice)
-print("Оптимальная цена: ", optPrice)
+def inputPersonRating():
+    return int(input("Ваш рейтинг: "))
+
+
+def accumulateStatistics(prices, ratings):
+    amountPriceRating = 0
+    amountRating = 0
+    for price, rating in zip(prices, ratings):
+        amountPriceRating += rating * price
+        amountRating += rating
+    return amountPriceRating, amountRating
+
+
+def computeOptimalPrice(personRating, amountPriceRating, amountRating):
+    optimalPrice = (((amountPriceRating * (amountRating + personRating))
+                     / amountRating) - amountPriceRating) / personRating
+    return int(optimalPrice)
+
+
+if __name__ == "__main__":
+    # считывание данных
+    prices, ratings = inputStatistics()
+    personRating = inputPersonRating()
+
+    # сбор статистики
+    amountPriceRating, amountRating = accumulateStatistics(prices, ratings)
+
+    # вычисление оптимальной цены
+    optimalPrice = computeOptimalPrice(personRating, amountPriceRating, amountRating)
+
+    print("Оптимальная цена: ", optimalPrice)
